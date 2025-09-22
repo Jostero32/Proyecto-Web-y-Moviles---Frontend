25e9092eef3a2f2e95584365bc7fbe647821431e
El Jonas <jona16lozada@gmail.com>
2025-09-22 15:10:30 -0500

    Se aadio el metodo para llamar al registro del backend con axios

--- src/services/api.js
+++ src/services/api.js
@@ -52,7 +52,10 @@ export const authAPI = {
     return response.data;
   },
 
-  
+  register: async (userData) => {
+    const response = await api.post('/users/register', userData);
+    return response.data;
+  },
 
   logout: () => {
     localStorage.removeItem('authToken');
