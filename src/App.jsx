afd1a53c2d879f99e588a3be807080b1e5a8b2a4
Reclax <marcoserrano2206@gmail.com>
2025-10-05 16:17:28 -0500

    Se actualiza las rutas de las paginas

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,10 @@ import Footer from './components/common/Footer';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import ProductosPage from './pages/ProductosPage';
+import VenderPage from './pages/VenderPage';
+import CategoriasPage from './pages/CategoriasPage';
+import ProductoDetallePage from './pages/ProductoDetallePage';
 
 function App() {
   return (
@@ -39,6 +43,10 @@ function Main() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/productos" element={<ProductosPage />} />
+          <Route path="/producto/:id" element={<ProductoDetallePage />} />
+          <Route path="/vender" element={<VenderPage />} />
+          <Route path="/categorias" element={<CategoriasPage />} />
         </Routes>
       </main>
 
